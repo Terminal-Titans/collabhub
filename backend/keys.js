f243d8235ec9a35f561d88cede06566af84a1394
HARSHIT MISHRA <20bec017@iiitdwd.ac.in>
2023-03-25 23:44:12 +0530

    createPost resolve

--- backend/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoUrl: "mongodb+srv://Monu:Lucifer%40666@mongo.dajlsqt.mongodb.net/test?authSource=mongo&authMechanism=SCRAM-SHA-1",
-    Jwt_secret: "faslkfocvneofubdsah"
-
-};
--- backend/models/post.js
+++ backend/models/post.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const moment = require("moment");
-const { ObjectId } = mongoose.Schema.Types
+const { ObjectId } = mongoose.Schema.Types;
 
 const postSchema = new mongoose.Schema(
   {
@@ -35,39 +35,16 @@ const postSchema = new mongoose.Schema(
         },
       },
     ],
-    techStacks: [
-      {
-        name: {
-          type: String,
-          // required: true,
-        },
-      },
-    ],
-    // startDate: {
-    //   type: Date,
-    // },
-    // endDate: {
-    //   type: Date,
-    // },
+    techStacks: {
+      type: String,
+      // required: true,
+    },
     startDate: {
       type: Date,
-      // get: function(date) {
-      //   return moment(date).format("YYYY-MM-DD");
-      // },
-      // set: function(dateString) {
-      //   return moment(dateString, "YYYY-MM-DD").toDate();
-      // },
     },
     endDate: {
       type: Date,
-      // get: function(date) {
-      //   return moment(date).format("YYYY-MM-DD");
-      // },
-      // set: function(dateString) {
-      //   return moment(dateString, "YYYY-MM-DD").toDate();
-      // },
     },
-
     collaborators: {
       type: Number,
       // required: true,
@@ -77,5 +54,4 @@ const postSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-mongoose.model("POST", postSchema)
-
+mongoose.model("POST", postSchema);
