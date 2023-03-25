23bc36a75c374ca51c85e199d28c43480ee2bb2e
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-03-26 04:58:38 +0530

    profile form updated

--- frontend/src/components/Profie.jsx
+++ frontend/src/components/Profie.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import PostDetail from "./PostDetail";
 import "./Profile.css";
+import "./profileform.css";
 import ProfilePic from "./ProfilePic";
 
 export default function Profie() {
-  var picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
+  var picLink = "https://cdn-icons-png.flaticon.com/128/3177/3177440.png";
   const [pic, setPic] = useState([]);
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   const [posts, setPosts] = useState([]);
-  const [user, setUser] = useState("")
-  const [changePic, setChangePic] = useState(false)
-
-
+  const [user, setUser] = useState("");
+  const [changePic, setChangePic] = useState(false);
   const toggleDetails = (posts) => {
     if (show) {
       setShow(false);
@@ -23,24 +22,28 @@ export default function Profie() {
 
   const changeprofile = () => {
     if (changePic) {
-      setChangePic(false)
+      setChangePic(false);
     } else {
-      setChangePic(true)
+      setChangePic(true);
     }
-  }
-
+  };
 
   useEffect(() => {
-    fetch(`http://localhost:5000/user/${JSON.parse(localStorage.getItem("user"))._id}`, {
-      headers: {
-        Authorization: "Bearer " + localStorage.getItem("jwt"),
-      },
-    })
+    fetch(
+      `http://localhost:5000/user/${
+        JSON.parse(localStorage.getItem("user"))._id
+      }`,
+      {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("jwt"),
+        },
+      }
+    )
       .then((res) => res.json())
       .then((result) => {
-        console.log(result)
+        console.log(result);
         setPic(result.post);
-        setUser(result.user)
+        setUser(result.user);
         console.log(pic);
       });
   }, []);
@@ -49,21 +52,26 @@ export default function Profie() {
     <div className="profile">
       {/* Profile frame */}
       <div className="profile-frame">
-        {/* profile-pic */}
-        <div className="profile-pic">
-          <img
-            onClick={changeprofile}
-            src={user.Photo ? user.Photo : picLink}
-            alt=""
-          />
-        </div>
-        {/* profile-data */}
-        <div className="pofile-data">
-          <h1>{JSON.parse(localStorage.getItem("user")).name}</h1>
-          <div className="profile-info" style={{ display: "flex" }}>
-            {/* <p>{pic ? pic.length : "0"} posts</p>
+        <div className="profilename">
+          {/* profile-pic */}
+          <div className="profile-pic">
+            <img
+              onClick={changeprofile}
+              src={user.Photo ? user.Photo : picLink}
+              alt=""
+            />
+          </div>
+          {/* profile-data */}
+          <div
+            style={{ marginTop: "20px", marginLeft: "4rem" }}
+            className="pofile-data"
+          >
+            <h1>{JSON.parse(localStorage.getItem("user")).name}</h1>
+            <div className="profile-info" style={{ display: "flex" }}>
+              {/* <p>{pic ? pic.length : "0"} posts</p>
             <p>{user.followers ? user.followers.length : "0"} followers</p>
             <p>{user.following ? user.following.length : "0"} following</p> */}
+            </div>
           </div>
         </div>
       </div>
@@ -72,11 +80,72 @@ export default function Profie() {
           width: "90%",
 
           opacity: "0.8",
-          margin: "25px auto",
+          margin: "5px auto",
         }}
       />
+      {show &&
+        <PostDetail item={posts} toggleDetails={toggleDetails} />
+      }
+      {
+        changePic &&
+        <ProfilePic changeprofile={changeprofile} />
+      }
+      <div class="wrapper">
+        <div class="registration_form">
+          <div class="title">update profile</div>
+
+          <form>
+            <div class="form_wrap">
+              <div class="input_grp">
+                <div class="input_wrap">
+                  <label for="fname">Name</label>
+                  <input type="text" id="fname" />
+                </div>
+                <div class="input_wrap">
+                  <label for="lname">UserName</label>
+                  <input type="text" id="lname" />
+                </div>
+              </div>
+              <div class="input_grp">
+                <div class="input_wrap">
+                  <label for="fname">GFG</label>
+                  <input type="text" id="fname" />
+                </div>
+                <div class="input_wrap">
+                  <label for="lname">Leetcode</label>
+                  <input type="text" id="lname" />
+                </div>
+              </div>
+              <div class="input_wrap">
+                <label for="email">email</label>
+                <input type="text" id="email" />
+              </div>
+              <div class="input_wrap">
+                <label for="college">college</label>
+                <input type="text" id="email" />
+              </div>
+
+              <div class="input_wrap">
+                <label for="city">PortFolio-optional</label>
+                <input type="text" id="city" />
+              </div>
+              <div class="input_wrap">
+                <label for="country">LinkedIn</label>
+                <input type="text" id="country" />
+              </div>
+              <div class="input_wrap">
+                <label for="about">About</label>
+                <input type="text" id="country" />
+              </div>
+              <div class="input_wrap">
+                <input type="submit" value="Update" class="submit_btn" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
       {/* Gallery */}
-      <div className="gallery">
+      {/* <div className="gallery">
         {pic.map((pics) => {
           return <img key={pics._id} src={pics.photo}
             onClick={() => {
@@ -91,7 +160,7 @@ export default function Profie() {
       {
         changePic &&
         <ProfilePic changeprofile={changeprofile} />
-      }
+      } */}
     </div>
   );
 }
